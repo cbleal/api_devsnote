d5de2ee2a52600d9098e07a9d26905b609bd389e
Claudinei Borges Leal <cborgesleal@gmail.com>
2021-10-26 16:32:04 -0300

    Connection Class Created

--- config.php
+++ config.php
@@ -1,1 +1,10 @@
 <?php
+
+# conexão com o banco
+$db_host = 'localhost';
+$db_name = 'test';
+$db_user = 'root';
+$db_password = '';
+$dsn = "mysql:dbhost={$db_host};dbname={$db_name}";
+
+$pdo = new PDO($dsn, $db_user, $db_password);
