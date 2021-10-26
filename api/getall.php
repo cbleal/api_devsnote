f95dc0d5091cc66f8ea5c6793320eb93442a8fcb
Claudinei Borges Leal <cborgesleal@gmail.com>
2021-10-26 17:17:05 -0300

    Getall class created

--- api/getall.php
+++ api/getall.php
@@ -1,2 +1,25 @@
 <?php
 
+require '../config.php';
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+
+if ($method == 'get') :
+    $query = $pdo->query("SELECT * FROM devsnote");
+
+    if ($query->rowCount() > 0) :
+        $data = $query->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($data as $item) :
+            $array['result'][] = [
+                'id' => $item['id'],
+                'title' => $item['title']
+            ];
+        endforeach;
+    else :
+        $array['error'] = "Não existm dados";
+    endif;
+else :
+    $array['error'] = "Metodo invalido (Apenas GET)";
+endif;
+
+require '../return.php';
